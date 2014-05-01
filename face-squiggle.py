c0dcbbb1262377da46a6c52260e7fb396ae798a0
samepant <sampanter@gmail.com>
2014-04-30 21:12:42 -0700

    sort of liking where this is at, marking faces and szeliski objects

--- face-squiggle.py
+++ face-squiggle.py
@@ -1,18 +1,78 @@
 import sys, os
-from SimpleCV import Color
+#from PIL import Image
 from SimpleCV import *
+from SimpleCV import ImageClass
+from random import randint
 
-def facedetect(pic):
+
+
+
+def facesquiggle(pic):
 	faces = pic.findHaarFeatures("haarcascade_frontalface_alt2.xml")
 	for f in faces:
-		print "Face located at " + str(f.coordinates())
-	faces.draw(Color.GREEN)
-	pic.save("face_detect.jpg")
+		red = randint(245,255)
+		green = randint(245,255)
+		blue = randint(245,255)
+		facewidth = f.width()
+		facex = f.x - f.width()/2
+		facey = f.y - f.height()/2
+		pic.dl().selectFont('helvetica')
+		pic.drawText('FACE', facex, facey, (red,green,blue), facewidth/2)
+
+	
+				#was using this to draw my own image over detected features (decided to let the computer draw and further remove myself, more procedural)
+				#sqg = Image("sqg.png") #needs to be in the same directory
+				#sqgmask = sqg.createAlphaMask(hue=1)
+				#sqgmask = sqg.createBinaryMask(color1=(0, 0, 0),color2=(5,5,5))
+				#print sqg.height
+
+
+				#for f in faces:
+					#facewidth = f.width()
+					#faceheight = f.height()
+					#facex = f.x
+					#facey = f.y
+					#sqgx = f.x - f.width()/2
+					#sqgy = f.y - f.height()/2
+					#facesqg = sqg.adaptiveScale((facewidth, faceheight))
+					#sqgmaskscaled = sqgmask.adaptiveScale((facewidth, faceheight))
+					#print facesqg.height
+					#simplecv only method 
+					#pic = pic.blit(facesqg,pos=(sqgx,sqgy))
+					#This is a PIL version which has support for transparent .png
+					#pic = PIL.paste(sqg, (facesqg, sqgx, sqgy), mask=facesqg) 
+	#points = pic.findFeatures("harris", 1000)
+	#for p in points:
+	#	red = randint(1,255)
+	#	green = randint(1,255)
+	#	blue = randint(1,255)
+	#	p.draw((red, green, blue))
+
+	corners = pic.findCorners()
+	for c in corners:
+		red = randint(1,255)
+		green = randint(1,255)
+		blue = randint(1,255)
+		c.draw((red, green, blue))
+
+	points = pic.findFeatures("szeliski", 1000)
+	for p in points:
+		red = randint(1,255)
+		green = randint(1,255)
+		blue = randint(1,255)
+		pointx = p.x
+		pointy = p.y
+		p.draw((red,green,blue))	
+
+
+	pic.save("cv_1/%s" % (sys.argv[1]))
+	
+
 
 
 def main():
 	pic = Image(sys.argv[1])
-	facedetect(pic)
+	facesquiggle(pic)
 
 
 if __name__ == "__main__":
